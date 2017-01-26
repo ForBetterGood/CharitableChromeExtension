510321f10ec1fe084295d55c78272311eac4cd88
Alex Goodger <alex@atomlab.co.uk>
2017-01-26 20:18:28 +0000

    Perform redirect when Amazon UK pages are hit directly

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,11 +1,11 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
-        var detectURLs = ["https://www.amazon.co.uk/", "https://www.amazon.com/"];
-
-        detectURLs.forEach(function(URL) {
-            details.url = URL ? alert('Should redirect for charity\'s sake') : null
-        });
+        if (details.url == "https://www.amazon.co.uk/" || details.url == "https://www.amazon.com/gp/homepage.html") {
+            alert('AMAZON!');
+            return { redirectUrl: "https://google.com" };
+        };
     }, {
         urls: ["https://*/*"]
-    }
+    },
+    ["blocking"]
 );
