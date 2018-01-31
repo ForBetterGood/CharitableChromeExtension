66c87f3d57139069fe2ea1860e04688a43b65ddd
Alex Goodger <alex@atomlab.co.uk>
2018-01-31 17:01:00 +0000

    Add US support and some other slight improvements, also update manifest file for versioning

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,59 +1,83 @@
 chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
+  function(details) {
+    console.log(details);
+    var amazonUS = "https://www.amazon.com";
 
-        var charityLinks = [{
-            name: 'RNLI',
-            url: 'http://www.amazon.co.uk/gp/homepage.html/026-5210700-2589212?ie=UTF8&tag=rnliorguk-21&link_code=hom'
-        }, {
-            name: 'Macmillan',
-            url: 'http://www.amazon.co.uk/b?node=118856031&tag=macmcancsupps-21'
-        }, {
-            name: 'OCD-UK',
-            url: 'http://www.amazon.co.uk/exec/obidos/redirect-home?tag=ocduk02-21'
-        }, {
-            name: 'Allergy UK',
-            url: 'http://www.amazon.co.uk/b/ref=topnav_storetab_w_h_?ie=UTF8&node=468294&tag=allergyuk-21'
-        }, {
-            name: 'Beanstalk',
-            url: 'http://www.amazon.co.uk/?ie=UTF8&amp%3Bsite=amazon&link_code=hom&tag=volunteerread-21'
-        }, {
-            name: 'Saint Francis Hospice',
-            url: 'http://www.amazon.co.uk/s?me=A2ETR4ARX8KZ&merchant=A2ETR4ARX8KZ&qid=1432073141'
-        }, {
-            name: 'Hospice Aid UK',
-            url: 'http://www.amazon.co.uk/?&tag=hoaiuk-21'
-        }, {
-            name: 'Weldmar Hospicecare',
-            url: 'http://www.amazon.co.uk/?_encoding=UTF8&tag=weldmhospitru-21'
-        }, {
-            name: 'Families Need Fathers',
-            url: 'http://www.amazon.co.uk/exec/obidos/redirect-home?tag=wwwfnforguk-21&placement=home_multi.gif&site=amazon'
-        }, {
-            name: 'Epilepsy Society',
-            url: 'http://www.amazon.co.uk/exec/obidos/redirect-home?tag=thenationalsoc04&site=books'
-        }, {
-            name: 'Epilepsy Action',
-            url: 'http://www.amazon.co.uk/exec/obidos/redirect-home?tag=britishepilepsya&site=amazon'
-        }, {
-            name: 'Bumblebee Conservation Trust',
-            url: 'http://www.amazon.co.uk/?_encoding=UTF8&tag=bumblebeecons-21&linkCode=ur2&camp=1634&creative=6738'
-        }];
-
-        if (details.url == "https://www.amazon.co.uk/" || details.url == "https://www.amazon.com/gp/homepage.html") {
-            var charity = charityLinks[Math.floor(Math.random() * charityLinks.length)];
-
-            var notifyOptions = {
-                type: "basic",
-                title: "Donating to " + charity.name,
-                message: "If you order anything from Amazon in this session, at least 5% will go to " + charity.name + ". Go you!",
-                iconUrl: "icons/icon48.png"
-            }
-
-            chrome.notifications.create(notifyOptions);
-
-            return { redirectUrl: charity.url };
-        };
+    var charityLinks = [{
+      name: 'RNLI',
+      url: 'http://www.amazon.co.uk/gp/homepage.html/026-5210700-2589212?ie=UTF8&tag=rnliorguk-21&link_code=hom'
     }, {
-        urls: ["https://*/*"]
-    }, ["blocking"]
+      name: 'Macmillan',
+      url: 'http://www.amazon.co.uk/b?node=118856031&tag=macmcancsupps-21'
+    }, {
+      name: 'OCD-UK',
+      url: 'http://www.amazon.co.uk/exec/obidos/redirect-home?tag=ocduk02-21'
+    }, {
+      name: 'Allergy UK',
+      url: 'http://www.amazon.co.uk/b/ref=topnav_storetab_w_h_?ie=UTF8&node=468294&tag=allergyuk-21'
+    }, {
+      name: 'Beanstalk',
+      url: 'http://www.amazon.co.uk/?ie=UTF8&amp%3Bsite=amazon&link_code=hom&tag=volunteerread-21'
+    }, {
+      name: 'Saint Francis Hospice',
+      url: 'http://www.amazon.co.uk/s?me=A2ETR4ARX8KZ&merchant=A2ETR4ARX8KZ&qid=1432073141'
+    }, {
+      name: 'Hospice Aid UK',
+      url: 'http://www.amazon.co.uk/?&tag=hoaiuk-21'
+    }, {
+      name: 'Weldmar Hospicecare',
+      url: 'http://www.amazon.co.uk/?_encoding=UTF8&tag=weldmhospitru-21'
+    }, {
+      name: 'Families Need Fathers',
+      url: 'http://www.amazon.co.uk/exec/obidos/redirect-home?tag=wwwfnforguk-21&placement=home_multi.gif&site=amazon'
+    }, {
+      name: 'Epilepsy Society',
+      url: 'http://www.amazon.co.uk/exec/obidos/redirect-home?tag=thenationalsoc04&site=books'
+    }, {
+      name: 'Epilepsy Action',
+      url: 'http://www.amazon.co.uk/exec/obidos/redirect-home?tag=britishepilepsya&site=amazon'
+    }, {
+      name: 'Bumblebee Conservation Trust',
+      url: 'http://www.amazon.co.uk/?_encoding=UTF8&tag=bumblebeecons-21&linkCode=ur2&camp=1634&creative=6738'
+    }];
+
+    if (details.url == "https://www.amazon.co.uk/" || details.url == "https://www.amazon.com/gp/homepage.html") {
+      var charity = charityLinks[Math.floor(Math.random() * charityLinks.length)];
+
+      var notifyOptions = {
+        type: "basic",
+        title: "Donating to " + charity.name,
+        message: "If you order anything from Amazon in this session, at least 5% will go to " + charity.name + ". Go you!",
+        iconUrl: "icons/icon48.png"
+      }
+
+      chrome.notifications.create(notifyOptions);
+
+      return {
+        redirectUrl: charity.url
+      };
+    };
+
+    if (details.url.indexOf(amazonUS) > -1 && details.url.length <= amazonUS.length + 1) {
+
+      var notifyOptions = {
+        type: "basic",
+        title: "Donating via Amazon Smile",
+        message: "When you sign in and purchase, money will be donated from it via Amazon Smile. Go you!",
+        iconUrl: "icons/icon48.png"
+      }
+
+      chrome.notifications.create(notifyOptions);
+
+      var currentURL = details.url.toString();
+      var newURL = currentURL.replace("https://www.amazon.com", "https://smile.amazon.com")
+
+      return {
+        redirectUrl: newURL 
+      };
+    };
+
+  }, {
+    urls: ["https://*/*"]
+  }, ["blocking"]
 );
